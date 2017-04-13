c261eb03523c2763c1389ea63aa32e04e9384fb2
Zachary R McKee <zmckee@hawk.iit.edu>
2017-04-12 23:39:35 -0500

    Added beginning of the declarations of HuffmanTree and IntBitArray

---
+++ services/IntBitArray.py
@@ -0,0 +1,8 @@
+class IntBitArray:
+    # example:
+    # |FFFF|0F0F|FACE|...
+    class IntBit:
+        def __init__(self,val): # val should be passed as a binary values
+            self.val = val
+    
+            
